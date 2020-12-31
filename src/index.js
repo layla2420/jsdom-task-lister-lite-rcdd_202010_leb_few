6d2335b6e1bb5bde0375b612747e6753f9600d85
layla  hammoud <laylahammoud63@gmail.com>
2020-12-31 16:23:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-
+  form.addEventListener(submit,function(){
+  addTask()
+  })
 });
 
 let taskDiscription = document.getElementById('new-task-description')
 let newTask =taskDiscription.value
 let list= document.getElementById("tasks")
-form.addEventListener(submit,function(){
 
-})
 function addTask{
   list.innerHTML=`<li>{newTask}<li>`
+  
 }
