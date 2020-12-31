67c363c01259a4ea3c80dae3db0d293e189988dc
layla  hammoud <laylahammoud63@gmail.com>
2020-12-31 16:22:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
 });
+
+let taskDiscription = document.getElementById('new-task-description')
+let newTask =taskDiscription.value
+let list= document.getElementById("tasks")
+form.addEventListener(submit,function(){
+  
+})
+function addTask{
+  list.innerHTML=`<li>{newTask}<li>`
+}
+
