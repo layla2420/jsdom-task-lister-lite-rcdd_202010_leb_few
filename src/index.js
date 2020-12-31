10a2ccc57da87291987323b5d008b8b2b55d8752
layla  hammoud <laylahammoud63@gmail.com>
2020-12-31 16:56:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+let form=document.getElementById('create-task-form')
+let taskDiscription = document.getElementById('new-task-description')
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  form.addEventListener(submit,function(){
-  addTask()
-  event.preventDefault()
-  })
+  form.addEventListener(submit,  addTask())
 });
 
-let taskDiscription = document.getElementById('new-task-description')
-let newTask =taskDiscription.value
-let list= document.getElementById("tasks")
 
-function addTask{
-  list.innerHTML=`<li>{newTask}<li>`
+
+
+
+function addTask(event){
+  event.preventDefault()
+ const newTask = document.createElement("li")
+  newTask.innerHTML=taskDiscription.value
 
 }
+const appendNewTask = task => {
+  let list= document.getElementById("tasks")
+  list.appendChild(task);
+};
